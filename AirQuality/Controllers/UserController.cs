f8d97c96f5dd8e9b5fe8972558fff3c6bd06ea5f
EgeAtaCeylan <eata@sabanciuniv.edu>
2023-03-30 13:30:40 +0300

    CREATED USER CONTROLLER AND ADDED USER  REGISTIRATION FUNCTION

---
+++ AirQuality/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using AirQuality.DbTransferObjects;
+using AirQuality.Entities;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace AirQuality.Controllers
+{ 
+
+    [ApiController]
+    [Route("api/user/[controller]")]
+    public class UserController: ControllerBase
+	{
+        private readonly Ens4912AirqualityContext? _context;
+
+        public UserController(Ens4912AirqualityContext? context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost("RegisterUser")]
+        public async Task<HomeUser> UserRegistiration(HomeUser newUser)
+        {
+            HomeUser userSearch = await _context.HomeUsers.Select(s => new HomeUser
+            {
+                userId = s.userId,
+                HomeId = s.HomeId,
+                Username = s.Username,
+                Mail = s.Mail,
+                Password = s.Password,
+            }).FirstOrDefaultAsync(s => s.Mail == newUser.Mail);
+
+            if (userSearch == null)
+            {
+                var entity = new HomeUser()
+                {
+                    Username = newUser.Username,
+                    Mail = newUser.Mail,
+                    Password = newUser.Password,
+                    HomeId = newUser.HomeId
+                };
+                _context.HomeUsers.Add(entity);
+                await _context.SaveChangesAsync();
+                
+                return await _context.HomeUsers.Select(s => new HomeUser
+                {
+                    userId = s.userId,
+                    HomeId = s.HomeId,
+                    Username = s.Username,
+                    Mail = s.Mail,
+                    Password = s.Password,
+                }).FirstOrDefaultAsync(s => s.Mail == newUser.Mail);
+            }
+
+            else
+            {
+                return null;
+            }
+
+        }
+    }
+}
+
